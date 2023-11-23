e637ba1e4364741c530f3a0f7333423f2bde5150
mizonov-iv <mizonov.iv@gmail.com>
2023-11-23 23:28:34 +0500

    initial commit, navbar added

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let header = document.getElementById('myHeader');
+    let page = document.getElementById('page');
+    let openMenuButton = document.getElementById('openmenu');
+
+    window.addEventListener('scroll', function() {
+        page.classList.remove('menuopen');
+        if (window.scrollY >= 100) {
+            header.classList.add('sticky');
+        } else {
+            // header.classList.remove('sticky');
+        }
+    });
+
+    page.addEventListener('click', function() {
+        page.classList.remove('menuopen');
+        header.classList.add('sticky');
+    })
+
+    // Event listener to remove the sticky class when the button is clicked
+    openMenuButton.addEventListener('click', function() {
+        header.classList.remove('sticky');
+        page.classList.add('menuopen');
+    });
+
+    let links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            // Prevent the default action
+            event.preventDefault();
+
+            // Get the target element
+            let targetId = this.getAttribute('href');
+            let targetElement = document.querySelector(targetId);
+
+            // Smooth scroll to target
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 style="color: #ffffff">Home</h1>
+</template>
+
+<script setup>
+
+</script>
