ec1ff3468fe01eea5d676077d9faa8fda221bfc6
mizonov <mizonov.iv@gmail.com>
2023-11-27 16:27:45 +0500

    navbar animation added

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,13 +1,7 @@
 <template>
-  <h1 style="color: #ffffff;">Skills</h1>
+
+
 </template>
 
-<script>
-export default {
-  name: "SkillsView"
-}
+<script setup>
 </script>
-
-<style scoped>
-
-</style>
