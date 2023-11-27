1afc27d742168ec3069e65769d9a0d9f367a9e22
mizonov <mizonov.iv@gmail.com>
2023-11-27 17:16:14 +0500

    navbar animation added

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,7 +1,38 @@
 <template>
-
+  <div >
+    <img
+        class="raise"
+        :src="icon.icon_url"
+        v-for="icon in icons"
+        :key="icon.id"
+    />
+  </div>
 
 </template>
 
 <script setup>
+import {ref} from "vue";
+
+const icons = ref([
+  {
+    id: 1,
+    title: "",
+    icon_url: "https://skillicons.dev/icons?i=html"
+  },
+  {
+    id: 2,
+    title: "",
+    icon_url: "https://skillicons.dev/icons?i=css"
+  },
+  {
+    id: 3,
+    title: "",
+    icon_url: "https://skillicons.dev/icons?i=js"
+  },
+  {
+    id: 4,
+    title: "",
+    icon_url: "https://skillicons.dev/icons?i=html"
+  },
+])
 </script>
