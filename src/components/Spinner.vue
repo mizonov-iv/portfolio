c6d47c9884e8079bb2313002e627276933845435
mizonov-iv <mizonov.iv@gmail.com>
2023-11-29 23:20:04 +0500

    spinner added

---
+++ src/components/Spinner.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="spinner-wrapper" v-if="!isLoaded">
+    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+  </div>
+</template>
+
+<script setup>
+import {onMounted, ref} from "vue";
+
+const isLoaded = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoaded.value = true
+  }, 3000)
+})
+</script>
+
+<style scoped>
+.spinner-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  backdrop-filter: blur(15px);
+  animation: spinnerAnimation 2s forwards;
+  transition: all 0.5s ease-in-out;
+
+}
+
+.lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 33px;
+  width: 13px;
+  height: 13px;
+  border-radius: 50%;
+  background: #000000;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 8px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 8px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 32px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 56px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(24px, 0);
+  }
+}
+
+@keyframes spinnerAnimation {
+  0% {
+    opacity: 1;
+  }
+  /*25% {*/
+  /*  opacity: 0.75;*/
+  /*}*/
+  /*50% {*/
+  /*  opacity: 0.5;*/
+  /*}*/
+  /*75% {*/
+  /*  opacity: 0.25;*/
+  /*}*/
+  100% {
+    opacity: 0;
+  }
+}
+
+</style>
--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,4 +1,6 @@
 <template>
+  <Spinner/>
+
   <header id="myHeader" class="sticky">
     <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 714.6 401.1">
       <path fill="#fff" fill-rule="evenodd" d="M502.8 0h211.8l-23 39.7-138.5 240L483 401H342.7L413 279.6 251.4 0h140.3L483 158.1 538.6 62 502.8 0Zm-201 279.6L140.1 0H0l231.7 401 70-121.4Z"/>
@@ -18,6 +20,7 @@
 
 <script setup>
 import './navbar.js'
+import Spinner from "/src/components/Spinner.vue"
 
 function handleNavClick() {
   let page = document.getElementById('page');
